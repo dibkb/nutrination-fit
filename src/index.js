1c163eb6c892473b15fde5ba5841b47b10669b59
baroonjha <85762785+baroonjha@users.noreply.github.com>
2024-02-19 15:35:44 +0530

    added single blog page

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import './style.css'
 import Home from './views/home'
 import NotFound from './views/not-found'
 import Blogs from './components/Blogs'
+import BlogHiddenCost from './components/BlogPost/blogHiddenCost'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={Blogs} exact path="/blogs" />
+        <Route component={BlogHiddenCost} exact path="/blogpost"/>
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
